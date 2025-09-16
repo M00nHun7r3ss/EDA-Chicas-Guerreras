04caf7dcf5a22f03bfcd14d84655526ce19bd91a
si <carmengb27@ucm.es>
2025-09-16 16:54:34 +0200

    he de terminar el 1-2 coño

---
+++ TEMA1-2/TEMA1-2.cpp
@@ -0,0 +1,64 @@
+// Carmen Gómez Becerra
+// EDA-GDV29
+
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+
+
+// función que resuelve el problema
+TipoSolucion resolver(TipoDatos datos) {
+
+
+}
+
+// Resuelve un caso de prueba, leyendo de la entrada la
+// configuración, y escribiendo la respuesta
+void resuelveCaso() {
+    // leer los datos de la entrada
+    int n; 
+    std::cin >> n;
+
+    // PICOS Y VALLES:
+    // ej.: (a, b, c, d)
+    // · el primero no puedes combrobar el anterior.
+    // · el ultimo no puedes comprobar el posterior.
+    // · valle -> a > b && b < c
+    // · pico -> a < b && c < b
+
+    // tiene que recorrer toda la secuencia para ver sus picos y valles 
+    for (int i = 1; i < n - 1; i++) { // empezamos en el segundo y acabamos en el penultimo
+
+    }
+
+
+    TipoSolucion sol = resolver(datos);
+    // escribir sol
+
+
+}
+
+int main() {
+    // Para la entrada por fichero.
+    // Comentar para acepta el reto
+#ifndef DOMJUDGE
+    std::ifstream in("datos.txt");
+    auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
+#endif 
+
+    int numCasos;
+    std::cin >> numCasos;
+    for (int i = 0; i < numCasos; ++i)
+        resuelveCaso();
+
+
+    // Para restablecer entrada. Comentar para acepta el reto
+#ifndef DOMJUDGE // para dejar todo como estaba al principio
+    std::cin.rdbuf(cinbuf);
+    system("PAUSE");
+#endif
+
+    return 0;
+}
