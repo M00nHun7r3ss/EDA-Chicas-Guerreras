b27a4ceb808b9a4d996aa6fdd22562a6c714db06
acostacion <carmengb27@ucm.es>
2025-09-25 20:06:49 +0200

    lo dejo pa mañana estoy harta

---
+++ TEMA2-03/TEMA2-03.cpp
@@ -0,0 +1,68 @@
+// Carmen Gómez Becerra 
+// EDA - GDV29
+// Complejidad
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+
+
+/*
+// función que resuelve el problema
+TipoSolucion resolver(TipoDatos datos) {
+
+
+}
+*/
+
+// Resuelve un caso de prueba, leyendo de la entrada la
+// configuración, y escribiendo la respuesta
+bool resuelveCaso() {
+
+    // leer los datos de la entrada
+    int n;
+    std::cin >> n;
+
+    if (n == 0) return false;
+
+    while (n != 0)
+
+    
+    // Diremos que un vector esta parcialmente ordenado si:
+    // el valor maximo de su mitad derecha >= que todos los valores de la mitad izquierda
+    // &&
+    // el valor minimo de su mitad izquierda <= que todos los valores de su mitad derecha
+
+    // almacenamos en un vector la secuencia.
+    
+    
+    
+
+    // escribir sol
+
+    return true;
+
+}
+
+int main() {
+    // Para la entrada por fichero.
+    // Comentar para acepta el reto
+#ifndef DOMJUDGE
+    std::ifstream in("datos.txt");
+    auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
+#endif 
+
+
+    while (resuelveCaso())
+        ;
+
+
+    // Para restablecer entrada. Comentar para acepta el reto
+#ifndef DOMJUDGE // para dejar todo como estaba al principio
+    std::cin.rdbuf(cinbuf);
+    system("PAUSE");
+#endif
+
+    return 0;
+}
